be0db93ac046913050e1c7aaefbc38b4eb6cf490
Verdictus <flavius256@icloud.com>
2021-06-02 19:09:33 -0600

    Rearrange Directory

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace _291CarProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // myLabel
+            // 
+            this.myLabel.AutoSize = true;
+            this.myLabel.BackColor = System.Drawing.Color.Black;
+            this.myLabel.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.myLabel.ForeColor = System.Drawing.Color.Red;
+            this.myLabel.Location = new System.Drawing.Point(303, 184);
+            this.myLabel.Name = "myLabel";
+            this.myLabel.Size = new System.Drawing.Size(377, 50);
+            this.myLabel.TabIndex = 0;
+            this.myLabel.Text = "VrOoOoooMooooMo";
+            this.myLabel.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(45, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 237);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Car go";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.myLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label myLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
