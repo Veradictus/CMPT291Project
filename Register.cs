3c3c41a19c96b2b4e9f21a644c083915eb6468f8
Verdictus <flavius256@icloud.com>
2021-06-10 17:33:10 -0600

    Registration Start

--- Register.cs
+++ Register.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace _291CarProject
 {
-
-
     public partial class Register : Form
     {
         public Register()
@@ -34,16 +28,37 @@ namespace _291CarProject
 
         private void register_button_Click(object sender, EventArgs e)
         {
+            bool userExists = _291CarProject.Static.Database.UserExists(register_username_text.Text);
+
+            if (userExists)
+            {
+                MessageBox.Show("The username you have entered is already registered.");
+
+                return;
+            }
+
+            bool createUser = _291CarProject.Static.Database.CreateUser(register_username_text.Text, password_text.Text,
+                "Male", firstName.Text, lastName.Text, street.Text, city.Text, province.Text);
+
+            if (!createUser)
+            {
+                MessageBox.Show("An error has occurred whilst trying to create the user.");
+
+                return;
+            }
+
             this.Hide();
+
+            Login loginForm = new Login();
+
+            loginForm.Show();
+
+            /* this.Hide();
 
             MainMenu mainMenuForm = new MainMenu();
 
-            mainMenuForm.Show();
+            mainMenuForm.Show();*/
         }
 
-        private void Register_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
